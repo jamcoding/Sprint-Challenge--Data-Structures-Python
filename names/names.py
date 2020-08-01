d31e7dd941f9649399d42db9924bea94706e31a5
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-07-31 20:49:08 -0500

    added BSTNode class in names.py

--- names/names.py
+++ names/names.py
@@ -13,10 +13,42 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, name):
+        if self.value == name:
+            return True
+        if name < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(name)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
