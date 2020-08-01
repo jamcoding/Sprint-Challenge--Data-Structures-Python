f27f9b6214a88badbffbcfddbe9463bacaf7d298
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-07-31 20:19:34 -0500

    ring_buffer tests passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,20 @@ class RingBuffer:
         self.oldestItem = 0
 
     def append(self, item):
-        pass
+        # checking the length
+        if len(self.list) == self.capacity:
+            # if full, replace the oldest item
+            if self.oldestItem >= self.capacity:
+                print(self.list)
+                self.oldestItem = 0
+                self.list[self.oldestItem] = item
+            else:
+                self.list[self.oldestItem] = item
+
+            self.oldestItem += 1
+        # else - append item to list
+        else:
+            self.list.append(item)
 
     def get(self):
-        pass
+        return self.list
